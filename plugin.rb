f419cddd29970fa732a6730d42dc606da9aa81c1
Ben Soares <b.soares@dundee.ac.uk>
2021-06-17 13:26:31 +0100

    fix mapping split

--- plugin.rb
+++ plugin.rb
@@ -24,21 +24,23 @@ class CrowdAuthenticatorMode
       group_map[keyval[0]] = keyval[1]
       Rails.logger.warn("debug: crowd_groups: map='#{map}', keyval[0]='#{keyval[0]}', keyval[1]='#{keyval[1]}'")
     }
-    crowd_groups.each { |crowd_group|
-      Rails.logger.warn("debug: crowd_groups:   crowd_group='#{crowd_group}'")
-      if group_map.has_key?(crowd_group) || !SiteSetting.crowd_groups_remove_unmapped_groups
-        result = nil
-        discourse_groups = group_map[crowd_group]
-        Rails.logger.warn("debug: crowd_groups:     crowd_group='#{crowd_group}', discourse_groups='#{discourse_groups}'")
-        discourse_groups.split(",").each { |discourse_group|
-          Rails.logger.warn("debug: crowd_groups:     discourse_group='#{discourse_group}'")
-          actual_group = Group.find(discourse_group) if discourse_group
-          Rails.logger.warn("debug: crowd_groups:     discourse_group='#{discourse_group}', actual_group='#{actual_group}'")
-          result = actual_group.add(user) if actual_group
-          Rails.logger.error("debug: crowd_group '#{crowd_group}' mapped to discourse_group '#{discourse_group}' didn't get added to user.id '#{user.id}'") if !result
-        }
-      end
-    }
+    if !(crowd_groups.nil? || group_map.empty?)
+      crowd_groups.each { |crowd_group|
+        Rails.logger.warn("debug: crowd_groups:   crowd_group='#{crowd_group}'")
+        if group_map.has_key?(crowd_group) || !SiteSetting.crowd_groups_remove_unmapped_groups
+          result = nil
+          discourse_groups = group_map[crowd_group]
+          Rails.logger.warn("debug: crowd_groups:     crowd_group='#{crowd_group}', discourse_groups='#{discourse_groups}'")
+          discourse_groups.split(",").each { |discourse_group|
+            Rails.logger.warn("debug: crowd_groups:     discourse_group='#{discourse_group}'")
+            actual_group = Group.find(discourse_group) if discourse_group
+            Rails.logger.warn("debug: crowd_groups:     discourse_group='#{discourse_group}', actual_group='#{actual_group}'")
+            result = actual_group.add(user) if actual_group
+            Rails.logger.error("debug: crowd_group '#{crowd_group}' mapped to discourse_group '#{discourse_group}' didn't get added to user.id '#{user.id}'") if !result
+          }
+        end
+      }
+    end
   end
 
 end
